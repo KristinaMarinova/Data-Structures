090dd47bd75c6ca5a4074305e4dafddf8f24ba27
Kristina <kriska2000@abv.bg>
2020-09-24 21:21:24 +0300

    completed task

--- Fundamentals/04.HEAPS BST/Exercise/04.CookiesProblem/CookiesProblem.cs
+++ Fundamentals/04.HEAPS BST/Exercise/04.CookiesProblem/CookiesProblem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Wintellect.PowerCollections;
 
 namespace _04.CookiesProblem
 {
@@ -6,7 +7,27 @@ namespace _04.CookiesProblem
     {
         public int Solve(int k, int[] cookies)
         {
-            throw new NotImplementedException();
-        }
+           
+            var prioritySweetness = new OrderedBag<int>();
+
+            foreach (var sweetness in cookies)
+            {
+                prioritySweetness.Add(sweetness);
+            }
+
+            int currentCookie = prioritySweetness.GetFirst();
+            int operations = 0;
+
+            while (currentCookie < k && prioritySweetness.Count > 1)
+            {
+                int firstCookie = prioritySweetness.RemoveFirst();
+                int secondCookie = prioritySweetness.RemoveFirst();
+                int combinedCookie = firstCookie + (2 * secondCookie);
+                prioritySweetness.Add(combinedCookie);
+                currentCookie = prioritySweetness.GetFirst();
+                operations++;
+            }
+            return currentCookie >= k ? operations : -1;
+        }      
     }
 }
